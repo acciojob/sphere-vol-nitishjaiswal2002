9cceef21ac6e408a8567ba8d1d3b0dd1ff15d702
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2023-09-30 12:52:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
 function volume_sphere(e) {
-e.preventDefault();
-//alert("hi");
-	 // Get the radius input value
-            const radius = parseFloat(document.getElementById("radius").value);
-
-           if (isNaN(radius)) {
-               // Handle invalid input
-document.getElementById("volume").value = "Invalid input. Please enter a valid number.";
-        }
-		   else {
-                // Calculate the volume of the sphere
-               const calculate = (4/3) * Math.PI * Math.pow(radius, 3);
-calculate.toFixed(2);
-                // Display the calculated volume in the volume field
-                document.getElementById("volume").value =calculate;
-			   var  decimal1= calculate.toFixed(2);
+ const radius = parseFloat(document.getElementById("radius").value);
+            if (!isNaN(radius) && radius >= 0) {
+                const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+                document.getElementById("volume").value = volume.toFixed(4);
+            } else {
+                document.getElementById("volume").value = "NaN";
             }
-} 
-// it works yes yes yes ok sir ok ok
-//jo volume ayega uske 2 digit tk 
-//   it doesnt give output 
+	   e.preventDefault();
+        }
+
+ 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
